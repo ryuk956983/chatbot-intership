b83c629d967da06f1eb380b04521ed7f01805de5
ryuk956983 <106580255+ryuk956983@users.noreply.github.com>
2025-09-06 12:10:21 +0530

    Update connect.js

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connect = ()=>{
     try{
-mongoose.connect("mongodb+srv://vermaanurag550:kAB3kUJ4Jkf0JhDL@cluster0.e9wg5pp.mongodb.net/Internships?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://vermaanurag550:CADILLAC9569@cluster0.e9wg5pp.mongodb.net/Internships?retryWrites=true&w=majority&appName=Cluster0")
+
   console.log("Database connected");
     }catch(Err){
         console.log("Error Occured",Err);
